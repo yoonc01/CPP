a0a3047fcde5346caceb2fea5c1f962591918ce8
yoonc01 <yoonc01@hotmail.com>
2024-10-30 21:15:17 +0900

    using std::cout and std::string

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 
+using std::cout;
+using std::string;
+
 char	toUpper(char c)
 {
 	if (c >= 'a' && c <= 'z')
@@ -11,14 +14,14 @@ char	toUpper(char c)
 int	main(int ac, char **av)
 {
 	if (ac == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
 	{
 		for (int i = 1; i < ac; i++)
 		{
-			std::string str = av[i];
+			string str = av[i];
 			for (unsigned long j = 0; j < str.length(); j++)
-				std::cout << toUpper(str[j]);
+				cout << toUpper(str[j]);
 		}
 	}
 	return (0);
