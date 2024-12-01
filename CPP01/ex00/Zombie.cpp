8e6f30d0b78e3035ab1c0c6feef7d60af6eba53d
Hyojun Yoon <hyoyoon@cx2r8s4.42seoul.kr>
2024-12-01 19:29:43 +0900

    Zombie class done

---
+++ CPP01/ex00/Zombie.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Zombie.hpp"
+
+Zombie::Zombie() : _name("") {}
+
+Zombie::Zombie(std::string name) : _name(name) {}
+
+Zombie::~Zombie()
+{
+	std::cout << _name << " is dead." << std::endl;
+}
+
+void	Zombie::announce(void)
+{
+	std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+void	Zombie::setName(std::string name)
+{
+	this->_name = name;
+}
