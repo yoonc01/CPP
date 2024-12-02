2bb6ee73438a833643f9a58d3d3e4dc30da0e60d
Hyojun Yoon <hyoyoon@cx2r8s3.42seoul.kr>
2024-12-02 15:01:51 +0900

    ex03 done

--- CPP01/ex03/HumanB.cpp
+++ CPP01/ex03/HumanB.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
 #include <string>
-#include "HumanA.hpp"
+#include "HumanB.hpp"
 
-HumanA::HumanA(std::string name, Weapon &weapon) : _name(name), _weapon(weapon) {}
+HumanB::HumanB(std::string name) :_name(name), _weapon(nullptr) {}
 
-HumanA::~HumanA() {}
+HumanB::~HumanB() {}
 
-void	HumanA::attack()
+void	HumanB::attack()
 {
-	std::cout << _name << "attacks with " << _weapon.getType() << std::endl;
+	if (_weapon == nullptr)
+		std::cout << _name << " does not have any weapons" << std::endl;
+	else
+		std::cout << _name << " attacks with " << _weapon->getType() << std::endl;
+}
+
+void	HumanB::setWeapon(Weapon *weapon)
+{
+	if (weapon == nullptr)
+	{
+		std::cerr << "Error: Attempted to set a null weapon for " << _name << std::endl;
+		return ;
+	}
+	_weapon = weapon;
 }
