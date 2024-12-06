b3f472310f0217531fc248ee33acb70bfb9a5fc4
yoonc01 <yoonc01@hotmail.com>
2024-12-06 21:25:08 +0900

    CPP02 ex01 done

---
+++ CPP02/ex01/Fixed.hpp
@@ -0,0 +1,26 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+class   Fixed
+{
+    private:
+        int              _rawBits;
+        static const int _fractionBits;
+
+    public:
+        Fixed();
+        Fixed(const Fixed& obj);
+        Fixed&  operator=(const Fixed& obj);
+        ~Fixed();
+        Fixed(const int src);
+        Fixed(const float src);
+
+        int     getRawBits(void) const;
+        void    setRawBits(int const raw);
+        int     toInt(void) const;
+        float   toFloat(void) const;
+};
+
+std::ostream&   operator<<(std::ostream& os, const Fixed& obj);
+
+#endif
